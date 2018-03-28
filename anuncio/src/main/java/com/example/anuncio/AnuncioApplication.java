66eeea09de62b63ee301dd6387e06daf2df6a916
ivanpm16 <34659542+ivanpm16@users.noreply.github.com>
2018-03-28 17:17:50 +0200

    First Commit

---
+++ anuncio/src/main/java/com/example/anuncio/AnuncioApplication.java
@@ -0,0 +1,12 @@
+package com.example.anuncio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnuncioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnuncioApplication.class, args);
+	}
+}
